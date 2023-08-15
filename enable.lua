b9690c71e797e8d35256521b3390971db3fe8ca8
mpaulson <the.primeagen@gmail.com>
2023-08-15 11:40:53 -0600

    feat: enable lua.

--- enable.lua
+++ enable.lua
@@ -1,8 +1,3 @@
 function restart_htmx_lsp()
-    require("lsp-debug-tools").restart({
-        expected = {},
-        name = "htmx-lsp",
-        cmd = { "htmx-lsp", "--level", "DEBUG" },
-        root_dir = vim.loop.cwd(),
-    });
+    require("lsp-debug-tools").restart({ expected = {}, name = "htmx-lsp", cmd = { "htmx-lsp", "--level", "DEBUG" }, root_dir = vim.loop.cwd(), });
 end
